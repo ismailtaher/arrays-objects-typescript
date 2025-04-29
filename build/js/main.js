08970e4fb80d125fa54a313cf74c57f51ce7fdc2
ismailtaher <ismail.taher@ymail.com>
2025-04-29 18:06:45 +0500

    add starter code

---
+++ build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = "Ismail";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "Mirza";
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+let myName: string = "Ismail";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "Mirza";
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+let re: RegExp = /\w+/g;
