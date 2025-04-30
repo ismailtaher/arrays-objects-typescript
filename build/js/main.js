9368fb4cbdc96ee622abc62474acc604b92b1ba2
ismailtaher <ismail.taher@ymail.com>
2025-04-30 14:18:54 +0500

    understand Arrays & Tuples

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,7 @@
 "use strict";
+//
+// Arrays
+//
 let stringArr = ["one", "hey", "Ismail"];
 let guitars = ["Strat", "Les Paul", 5150];
 let mixedData = ["EVH", 1984, true];
@@ -10,3 +13,22 @@ guitars[0] = 1984;
 guitars.unshift("Jim");
 //stringArr = guitars;
 guitars = stringArr;
+let test = [];
+let bands = [];
+bands.push("van halen");
+//bands.push(false);
+//
+//Tuple
+//
+let myTuple = ["Ismail", 42, true];
+let mixed = ["Mirza", 1, false];
+mixed = myTuple;
+//myTuple = mixed;
+//myTuple[3] = 42;
+//myTuple[2] = 42
+//
+// Objects
+//
+let myObj;
+myObj = [];
+console.log(typeof myObj);
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+//
+// Arrays
+//
+
 let stringArr = ["one", "hey", "Ismail"];
 
 let guitars = ["Strat", "Les Paul", 5150];
@@ -17,3 +21,32 @@ guitars.unshift("Jim");
 //stringArr = guitars;
 
 guitars = stringArr;
+
+let test = [];
+let bands: string[] = [];
+bands.push("van halen");
+//bands.push(false);
+
+//
+//Tuple
+//
+
+let myTuple: [string, number, boolean] = ["Ismail", 42, true];
+
+let mixed = ["Mirza", 1, false];
+
+mixed = myTuple;
+
+//myTuple = mixed;
+
+//myTuple[3] = 42;
+
+//myTuple[2] = 42
+
+//
+// Objects
+//
+
+let myObj: object;
+myObj = [];
+console.log(typeof myObj);
